7e5ff4d3a1e5c6811f8a118c74d79cc27a409042
Rob Aimes <rob@aimes.dev>
2021-03-29 19:32:11 +0100

    Initial commit

---
+++ Model/Checkout/Design/ThreeStepExample/LayoutProcessor.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Copyright © Rob Aimes - https://aimes.dev/
+ * https://github.com/robaimes
+ */
+
+namespace Aimes\CheckoutDesignsExample\Model\Checkout\Design\ThreeStepExample;
+
+use Magento\Checkout\Block\Checkout\LayoutProcessorInterface;
+
+class LayoutProcessor implements LayoutProcessorInterface
+{
+    /**
+     * @inheritDoc
+     */
+    public function process($jsLayout): array
+    {
+        return $jsLayout;
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# Aimes_CheckoutDesignsExample
+
+This package exists purely to provide an example of how to utilise [`Aimes_CheckoutDesigns`](https://github.com/robaimes/module-checkout-designs)
